46eb268a9d83a0371afb8e85de6fcaeace23fba8
luis <luis.scholars@gmail.com>
2019-01-22 15:20:04 -0500

    Updates

---
+++ precache-manifest.794dc652e229fc5b6fc4a7c1e6cbf359.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fe16f0c6ca33e1cf17a62f51e563621",
+    "url": "/site/static/media/luis_castillo_resume .5fe16f0c.pdf"
+  },
+  {
+    "revision": "4fc79b2b14ed924a34ce",
+    "url": "/site/static/css/main.3a5d30e5.chunk.css"
+  },
+  {
+    "revision": "1627f8dec05cae81017d559117455c0a",
+    "url": "/site/static/media/dang.1627f8de.png"
+  },
+  {
+    "revision": "218c77d34c6d4cadf033",
+    "url": "/site/static/js/1.218c77d3.chunk.js"
+  },
+  {
+    "revision": "5571d5242120e713b47d",
+    "url": "/site/static/js/runtime~main.5571d524.js"
+  },
+  {
+    "revision": "4fc79b2b14ed924a34ce",
+    "url": "/site/static/js/main.4fc79b2b.chunk.js"
+  },
+  {
+    "revision": "f4b50e165be8058fec9d30105c40a360",
+    "url": "/site/static/media/profilePic.f4b50e16.png"
+  },
+  {
+    "revision": "dc2633b5c407669963bb6f5370647caa",
+    "url": "/site/static/media/sprtlook-1.dc2633b5.svg"
+  },
+  {
+    "revision": "b9bce4a02ac0d64ffeddec1a0a6b7a34",
+    "url": "/site/static/media/ProjectTwo.b9bce4a0.mp4"
+  },
+  {
+    "revision": "218c77d34c6d4cadf033",
+    "url": "/site/static/css/1.0dabb13e.chunk.css"
+  },
+  {
+    "revision": "1ecf15d15784415674d65dd40ce93efd",
+    "url": "/site/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Personal-Website/precache-manifest.000c2fd518c956ce656695da778f3f96.js"
+  "/site/precache-manifest.794dc652e229fc5b6fc4a7c1e6cbf359.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Personal-Website/index.html", {
+workbox.routing.registerNavigationRoute("/site/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
